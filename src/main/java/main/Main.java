77b3dc05ed4f6e39d115f312aa12e210fe19de8f
David Westlund <david.westlund@yaelev.se>
2022-04-07 11:34:27 +0200

    First scene javaFX working

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,22 @@
 package main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("UI/main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("SAKILA");
+        stage.setScene(scene);
+        stage.show();
     }
 }
